16ce181d244924b593bf11d09c1d344519d52144
AqsaRasheed08 <aqsarasheed.r26@gmail.com>
2023-04-02 23:56:32 +0500

    order schema

--- models/order.js
+++ models/order.js
@@ -1,11 +1,46 @@
 const mongoose = require('mongoose')
 
 const orderSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock:{
+    orderItems:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'OrderItem',
+        required: true
+    }],
+    ShippingAddress1:{
+        type: String,
+        required: true
+    },
+    ShippingAddress2:{
+        type: String,
+    },
+    city:{
+        type: String,
+        required: true
+    },
+    zip:{
+        type: String,
+        required: true
+    },
+    country:{
+        type: String,
+        required: true
+    },
+    phone:{
+        type: String,
+        required: true,
+        default: 'Pending',
+    },
+    totalPrice:{
         type: Number,
-        require: true
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    dateOrdered:{
+        type: Date,
+        default: Date.now(),
     }
 })
 
--- routes/orders.js
+++ routes/orders.js
@@ -1,4 +1,4 @@
-const {order} = require('../models/order');
+const {order, Order} = require('../models/order');
 const express = require('express');
 const { route } = require('./categories');
 const router = express.Router();
@@ -11,4 +11,51 @@ router.get('/' , async (req, res)=>{
     }
     res.send(orderList);
 })
+
+router.post('/', async (req, res)=>{
+    const orderItemsIds = Promise.all(req.body.orderItems.map(async orderItem =>{
+        let newOrderItem = new OrderItem({
+            quantity: orderItem.quantity,
+            product: orderItem.product
+        })
+        newOrderItem = await newOrderItem.save();
+
+        return newOrderItem._id;
+    }))
+    const orderItemsIdsResolved = await orderItemsIds;
+    console.log(orderItemsIdsResolved);
+
+    let order = new Order({
+        orderItems: orderItemsIdsResolved,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: req.body.totalPrice,
+        user: req.body.user
+    })
+// postmen video 14:40
+// localhost:5000/orders
+// go for authentication  and then copy token
+    // "orderItems": " ",
+    // "shippingAddress1": " ",
+    // "shippingAddress2": " ",
+    // "city": " ",
+    // "zip": " ",
+    // "country": " ",
+    // "phone": " ",
+    // "status": " ",
+    // "totalPrice": " ",
+    // "user": " "
+    order = await order.save();
+
+    if(!order)
+    return res.status(404).send('the order cannot be created')
+
+    res.send(order);
+})
+
 module.exports= router;
